fce0a63dc784c768a59005a2c03c20b758412ca9
jakbonful <johnalexbonful@gmail.com>
2026-05-21 13:30:22 +0000

    RTL

---
+++ rtl/fifo_sync.v
@@ -0,0 +1,57 @@
+`timescale 1us/1ns
+module fifo_sync #(
+    parameter FIFO_DEPTH = 8,  // no. of storage locations
+    parameter DATA_WIDTH = 32 )( // bits each storage can hold
+    input clk,
+    input rst_n,
+    input cs, 
+    input wr_en,
+    input rd_en,
+    input [DATA_WIDTH-1:0] data_in,
+    output reg [DATA_WIDTH-1:0] data_out,
+    output empty,
+    output full
+);
+
+localparam FIFO_DEPTH_LOG = $clog2(FIFO_DEPTH);
+    
+// Memory Element with 8 Words each 32 bits
+reg [DATA_WIDTH-1:0] fifo [0:FIFO_DEPTH-1];
+
+// wr/rd pointer have 1 extra bits at MSB
+reg [FIFO_DEPTH_LOG:0] write_pointer;
+reg [FIFO_DEPTH_LOG:0] read_pointer;
+
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+        write_pointer <= 0;
+    else if (cs && wr_en && !full)
+        write_pointer <= write_pointer + 1'b1; 
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+        read_pointer <= 0;
+    else if (cs && rd_en && !empty)
+        read_pointer <= read_pointer + 1'b1;
+end
+
+// Empty/Full Logic
+    assign empty = (read_pointer == write_pointer);
+    assign full = (read_pointer == {~write_pointer[FIFO_DEPTH_LOG],write_pointer[FIFO_DEPTH_LOG-1:0]});
+
+
+always @(posedge clk ) begin
+    if (cs && wr_en && !full)
+        fifo[write_pointer[FIFO_DEPTH_LOG-1:0]] <= data_in;    
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+        data_out <= 0;
+    else if (cs && rd_en && !empty)
+        data_out <= fifo[read_pointer[FIFO_DEPTH_LOG-1:0]];
+end
+
+endmodule
